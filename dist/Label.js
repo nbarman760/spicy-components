043056644d4fdc6be93a15a262d4bee99627cb31
Nandan Kumar Barman <nbarman760@gmail.com>
2023-09-15 00:22:04 +0530

    First Commit

---
+++ dist/Label.js
@@ -0,0 +1,8 @@
+import './style.css';
+export const Label = ({
+  text
+}) => {
+  return /*#__PURE__*/React.createElement("label", {
+    className: "spicy__label"
+  }, text);
+};
---
+++ src/components/Label.jsx
@@ -0,0 +1,6 @@
+import './style.css';
+export const Label= ({text})=>{
+    return (
+        <label className="spicy__label">{text}</label>
+    )
+}

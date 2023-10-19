e5783937768f00a399af5da41a73b8f5d52040f1
Nandan Kumar Barman <nbarman760@gmail.com>
2023-10-19 13:45:33 +0530

    update loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,21 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './loader.css';
 const Loader = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate loading with a delay
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
   return (
-    <div className={`loading-spinner ${loading ? 'show' : 'hide'}`}>
+    <div className={`loading-spinner`}>
       <div className="spinner"></div>
     </div>
   );

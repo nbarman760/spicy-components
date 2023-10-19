1038af03d1749924f3ded624d82748abfa0cfeb3
Nandan Kumar Barman <nbarman760@gmail.com>
2023-10-19 13:01:16 +0530

    update loader design

--- src/Loader.js
+++ src/Loader.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './loader.css';
 const Loader = () => {
   const [loading, setLoading] = useState(true);
-
+  const loaderRows = [];
   useEffect(() => {
     // Simulate loading with a delay
+    for (let i = 0; i < 15; i++) {
+      loaderRows.push('');
+    }
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2000);
@@ -17,41 +20,21 @@ const Loader = () => {
   return (
     <div classNameName={`loading-spinner ${loading ? 'show' : 'hide'}`}>
       <div className="wrapper">
-        <div className="wrapper-cell">
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-        </div>
-        <div className="wrapper-cell">
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-        </div>
-        <div className="wrapper-cell">
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-        </div>
-        <div className="wrapper-cell">
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-        </div>
-        <div className="wrapper-cell">
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-             <div className='cell'></div>
-        </div>
+        <div className='full-header'></div>
+        {
+          loaderRows.map((row) => (<div className="wrapper-cell">
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+            <div className='cell'></div>
+          </div>))
+        }
       </div>
     </div>
   );

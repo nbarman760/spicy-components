43db8d1e4f8704469c21d05d2c0403af4dc8f5e8
Nandan Kumar Barman <nbarman760@gmail.com>
2023-11-01 14:01:38 +0530

    update Textbox

--- src/Textbox.js
+++ src/Textbox.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { Form } from 'react-bootstrap';
 import React from 'react';
 
 import './style.css';
@@ -19,18 +18,18 @@ const Textbox = (props)=> {
     }
     return (
         <>
-        <Form.Group controlId={props.name}>
-            <Form.Label> {props.required ? '*': ''} {props.label}</Form.Label>
-            <Form.Control
-                type={props.type}
-                placeholder={props.placeholder ? props.placeholder : props.label}
-                name={props.name}
-                value={props.value}
-                autoComplete='off'
-                onChange={handleInputChange}
-                className={isValid ? '': 'invalid'}
-            />
-        </Form.Group>
+            <div className="textbox__container" id={props.name}>
+                <input
+                    type="text"
+                    className="outline-text-input"
+                    placeholder={props.placeholder ? props.placeholder : props.label}
+                    name={props.name}
+                    value={props.value}
+                    autoComplete='off'
+                    onChange={handleInputChange}
+                />
+                <label className="placeholder-label">{props.label}</label>
+            </div>
         </>
     )
 }

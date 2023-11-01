623a949782b37a3e00b8a65dbcef4673e30a04f0
Nandan Kumar Barman <nbarman760@gmail.com>
2023-11-01 16:17:59 +0530

    design fixed context menu

--- src/ContextMenu.js
+++ src/ContextMenu.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function ContextMenu({ top, left, onClose, onEdit, onDelete }) {
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
-    <div
-      className="context-menu"
-      style={{ top, left }}
-    >
+    <div className="context-menu" style={{ top, left }} ref={menuRef}>
       <ul>
         <li onClick={onEdit}>Edit</li>
         <li onClick={onDelete}>Delete</li>
